79676c9dc91e893693f9d5a31f208cabf0034674
khubiarov <72666523+khubiarov@users.noreply.github.com>
2023-07-25 12:15:14 +0800

    almost complete fbv fbv

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from blog.views import BlogListView, BlogCreatView, BlogDetailView, BlogUpdateView, BlogDeleteView
-from catalog.views import ContactView, HomeView
+from blog.views import BlogListView, BlogCreatView, BlogDetailView, BlogUpdateView, BlogDeleteView, published_func, \
+    FullListView
 
 app_name = 'blog'
 
@@ -9,5 +9,7 @@ urlpatterns = [
     path('create/', BlogCreatView.as_view(), name='create'),
     path('view/<int:pk>', BlogDetailView.as_view(), name='view'),
     path('edit/<int:pk>', BlogUpdateView.as_view(), name='edit'),
-    path('delete/<int:pk>', BlogDeleteView.as_view(), name='delete')
+    path('delete/<int:pk>', BlogDeleteView.as_view(), name='delete'),
+    path('full_list/', FullListView.as_view(), name='full_list'),
+    path('publishing/<int:pk>', published_func, name='published_func')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from datetime import date
-from django.views.generic import ListView, TemplateView, CreateView, UpdateView, DeleteView, DetailView
-
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+#, TemplateView оставил для других проектов
 from pytils.translit import slugify
 from django.urls import reverse_lazy, reverse
 from blog.models import Blog
+from django.shortcuts import get_object_or_404, redirect
 
 
 class BlogCreatView(CreateView):
@@ -13,11 +14,27 @@ class BlogCreatView(CreateView):
     fields = ('heading', 'content', 'preview',)
     success_url = reverse_lazy('blog:start_list')  # Здесь вроде переделать
 
-    def get_object(self, queryset=None):
-        self.object = super().get_object(queryset)
-        self.object.date_of_create = date.today()
-        self.object.save()
-        return self.object
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.heading)
+            new_blog.date_of_create = date.today()
+            new_blog.save()
+
+        return super().form_valid(form)
+
+
+def published_func(request, pk):
+    student_item = get_object_or_404(Blog, pk=pk)
+    if student_item.is_published:
+        student_item.is_published = False
+
+    else:
+        student_item.is_published = True
+
+    student_item.save()
+
+    return redirect(reverse('blog:full_list'))
 
 
 class BlogDetailView(DetailView):
@@ -38,8 +55,15 @@ class BlogListView(ListView):
 class BlogUpdateView(UpdateView):
     model = Blog
     fields = ('heading', 'content', 'preview',)
-    success_url = reverse_lazy('blog:view')# Не работает
+
+    def get_success_url(self):
+        return reverse('blog:view', args=[self.kwargs.get('pk')])
+
 
 class BlogDeleteView(DeleteView):
     model = Blog
     success_url = reverse_lazy('blog:start_list')
+
+class FullListView(ListView):
+    model = Blog
+    template_name = 'blog/blog_full_list.html'
